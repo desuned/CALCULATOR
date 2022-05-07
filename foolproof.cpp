d1bed24e22df0ccf0dc3e0661846f49afaa558b6
arst0tzka <kaarst@ya.ru>
2022-05-07 15:48:43 +0300

    thinked up a 4 cases that could break the program

---
+++ foolproof.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
+typedef string& sLink;
+
+int EqualBrackets(sLink equation) {
+	
+	return 1; 
+}
+int NoIncorrectSymbols(sLink equation) {
+
+	return 1;
+}
+int NoFollowingOperations(sLink equation) {
+	
+	return 1;
+}
+int TrigonometryIsCorrect(sLink equation) {
+	
+
+
+	return 1;
+}
+
+
+
+int main() {
+	string testEquation = "(5+6)-4*2^16*0+sin(0.6)\n";
+	
+
+	system("pause");
+}
