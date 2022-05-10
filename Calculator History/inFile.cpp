91adf303b1dcb57367e76c8108b04295e652be72
OneCloudInSky <gleb_zvor@mail.ru>
2022-05-10 16:12:57 +0300

    inFile() function is added in semi-full format, seeHistory() function is added, createFile() function is added, clearFile() function is added

--- Calculator History/inFile.cpp
+++ Calculator History/inFile.cpp
@@ -4,16 +4,63 @@
 
 using namespace std;
 
-void inFile(string initExp, int result) { //ввод в файл изначального выражения(initial expression) и результата(result)
-	ofstream  fout("Calculator History.txt");
+int hCounter = 0;
+
+int inFile(string initExp, string result) { // ввод в файл изначального выражения(initial expression) и результата(result)
+	ofstream  calcFile("Calculator History.txt", ios_base::app);
 	
-	fout << initExp << ' ' << result << "\n";
-	fout << "string" << 5;
+	if (!calcFile.is_open())  {
+		cout << "\nFile opening error accused!\n";
+		return 0;
+	}
+	
+	hCounter++;
+	
+	calcFile << hCounter << ". "<< initExp << ' ' << result << "\n";
+	
+	calcFile.close();
 }
 
-int main() {
-	inFile("sfaf", 6);
+void seeLast() { // вывод на экран последнего действия 
 	
+}
+
+string getInit() { //  функция, находящая нужное выражение по номеру в истории!!!
+	//
+}
+
+void seeHistory() { // вывод на экран всей истории
+	ifstream calcFile("Calculator History.txt");
+	
+	if (!calcFile.is_open()) cout << "\n   #The history was cleaned or non existing#   \n";
+	
+	else {
+		while (calcFile) {
+			string line;
+			
+			getline(calcFile, line);
+			
+			if (line == "") break;
+			else cout << line << "\n";
+		}
+		
+		calcFile.close();
+	}
+}
+
+int createFile() { // функция первоначального создания файла(необязательна)
+	ofstream calcFile("Calculator History.txt");
+	calcFile.close();
+}
+
+void clearFile() { //полная очистка истории
+	ofstream calcFile("Calculator History.txt", ios_base::trunc);
+}
+
+
+
+int main() { // основная функция для тестирования
+	return 0;
 }
 
 
