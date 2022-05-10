9daf503a771e715c0e532d7693b09b0ed8de08ba
OneCloudInSky <gleb_zvor@mail.ru>
2022-05-10 18:22:10 +0300

    getInit() function realization added, getLast() function realization added, only works for cycled programm, clearHistory() function fixed

--- Calculator History/inFile.cpp
+++ Calculator History/inFile.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <regex>
 
 using namespace std;
 
 int hCounter = 0;
 
+void Replace(string& s, string pattern, string replacer) {
+	s = regex_replace(s, regex{ pattern }, replacer);
+}
+
 int inFile(string initExp, string result) { // ввод в файл изначального выражения(initial expression) и результата(result)
 	ofstream  calcFile("Calculator History.txt", ios_base::app);
 	
 	if (!calcFile.is_open())  {
-		cout << "\nFile opening error accused!\n";
+		cout << "\n   #File opening error accused!#   \n";
 		return 0;
 	}
 	
@@ -21,12 +26,52 @@ int inFile(string initExp, string result) { // ввод в файл изначального выражени
 	calcFile.close();
 }
 
-void seeLast() { // вывод на экран последнего действия 
+string getLast() { // вывод на экран последнего действия 
+	ifstream calcFile("Calculator History.txt");
 	
+	if (!calcFile.is_open()) cout << "\n   #File opening error or non existing#   \n";
+	
+	else {
+		string tmpLine, lastLine;
+		
+		while (calcFile) {
+			
+			getline(calcFile, tmpLine);
+			
+			if (tmpLine[0] - 'a' + '1' == hCounter) {
+				Replace(tmpLine, "[0-9.]+$|^[0-9]+[.]{1}[ ]{1}", "");
+				lastLine = tmpLine;
+				break;
+			}
+			
+		}
+		
+		return lastLine;
+	}
 }
 
-string getInit() { //  функция, находящая нужное выражение по номеру в истории!!!
-	//
+string getInit(int needed) { //  функция, находящая нужное выражение по номеру в истории!!!
+	ifstream calcFile("Calculator History.txt");
+	
+	if (!calcFile.is_open()) cout << "\n   #File opening error or non existing#   \n";
+	
+	else {
+		string tmpLine, neededLine;
+		
+		while (calcFile) {
+			
+			getline(calcFile, tmpLine);
+			
+			if (tmpLine[0] - 'a' + '1' == needed) {
+				Replace(tmpLine, "[0-9.]+$|^[0-9]+[.]{1}[ ]{1}", "");
+				neededLine = tmpLine;
+				break;
+			}
+			
+		}
+		
+		return neededLine;
+	}
 }
 
 void seeHistory() { // вывод на экран всей истории
@@ -53,14 +98,10 @@ int createFile() { // функция первоначального создания файла(необязательна)
 	calcFile.close();
 }
 
-void clearFile() { //полная очистка истории
+void clearHistory() { //полная очистка истории
 	ofstream calcFile("Calculator History.txt", ios_base::trunc);
+	hCounter = 0;
 }
 
-
-
-int main() { // основная функция для тестирования
-	return 0;
-}
 
 
