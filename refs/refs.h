49dd30bfee4623dbb3b52f19b7f07e8bd7a6292a
Daniil <danya_zaikin1010@mail.ru>
2022-05-13 03:29:20 +0300

    Added functions in refs.h: RegexFind(); Replace()

--- refs/refs.h
+++ refs/refs.h
@@ -5,6 +5,33 @@
 #ifndef CALCULATOR_REFS_H
 #define CALCULATOR_REFS_H
 
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <regex>
+#include <vector>
+#include <stack>
+#include <sstream>
+#include <iomanip>
 
+using namespace std;
+
+typedef string& sLink;
+typedef string* sPtr;
+
+int RegexFind(string s, string pattern) {
+
+    smatch m;
+    regex rgxx(pattern);
+    regex_search(s, m, rgxx);
+    return !m.empty();
+
+}
+
+void Replace(sLink s, string pattern, string replacer) {
+
+    s = regex_replace(s, regex{ pattern }, replacer);
+
+}
 
 #endif //CALCULATOR_REFS_H
